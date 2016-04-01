93b457d07439bd8672d4ac5d0982748b674fd2df
drakezhard <drakezhard@gmail.com>
2016-03-31 21:16:07 -0700

    Better colors.

--- src/cljs/tic_tac_toe/colors.cljs
+++ src/cljs/tic_tac_toe/colors.cljs
@@ -1,7 +1,7 @@
 (ns tic-tac-toe.colors)
 
 (def base "rgb(27,62,64)")
-(def player1 "rgb(171, 136, 97)")
-(def player2 "rgb(196,170,97)")
+(def player1 "white")
+(def player2 "black")
 (def winner "rgb(241,117,118)")
 (def tiles "rgb(51,153,128)")
--- src/cljs/tic_tac_toe/mutations.cljs
+++ src/cljs/tic_tac_toe/mutations.cljs
@@ -4,57 +4,70 @@
 
 (defmulti mutate om/dispatch)
 
+(defn play [state index]
+  (fn []
+    (when-let [active (get-in @state [:active-player :idx])]
+      (u/next-move state active index)
+      (u/next-state state active))))
+
 (defmethod mutate 'tic-tac-toe/play
   [{:keys [state]} _ {:keys [index]}]
-  {:action
-   (fn []
-     (when-let [active (get-in @state [:active-player :idx])]
-       (u/next-move state active index)
-       (u/next-state state active)))})
+  {:action (play state index)})
+
+(defn computer-move [state idx]
+  (fn []
+    (when-let [active (get-in @state [:active-player :idx])]
+      (when (= active idx)
+        (let [board (u/get-board state)
+              move (u/choose-move state board idx)]
+          (u/next-move state idx move)
+          (u/next-state state idx))))))
 
 (defmethod mutate 'tic-tac-toe/computer-move
   [{:keys [state]} _ {:keys [idx]}]
   {:value {:keys [:idx :name :type :active? :winner :piece]}
-   :action
-   (fn []
-     (when-let [active (get-in @state [:active-player :idx])]
-       (when (= active idx)
-        (let [board (u/get-board state)
-              move (u/choose-move state board idx)]
-          (u/next-move state idx move)
-          (u/next-state state idx)))))})
+   :action (computer-move state idx)})
+
+(defn start [state]
+  (fn []
+    (swap! state update-in
+           [:active-player :idx]
+           (fn [] 1))
+    (swap! state update-in
+           [:player/by-index 1 :active?]
+           (fn [] true))
+    (swap! state update-in
+           [:player/by-index 2 :active?]
+           (fn [] false))))
 
 (defmethod mutate 'tic-tac-toe/start
   [{:keys [state]} _ _]
-  {:action (fn []
-             (swap! state update-in
-                    [:active-player :idx]
-                    (fn [] 1))
-             (swap! state update-in
-                    [:player/by-index 1 :active?]
-                    (fn [] true))
-             (swap! state update-in
-                    [:player/by-index 2 :active?]
-                    (fn [] false)))})
+  {:action (start state)})
+
+(defn restart [state]
+  (fn []
+    (mapv #(swap! state update-in
+                  [:player/by-index % :winner]
+                  (fn [] nil))
+          [1 2])
+    (mapv #(swap! state update-in
+                  [:tile/by-index % :value]
+                  (fn [] nil))
+          [[1 0] [0 1] [1 2] [2 1] [0 0] [2 0] [0 2] [2 2] [1 1]])))
 
 (defmethod mutate 'tic-tac-toe/restart
   [{:keys [state]} _ _]
-  {:action (fn []
-             (mapv #(swap! state update-in
-                       [:player/by-index % :winner]
-                       (fn [] nil))
-                   [1 2])
-             (mapv #(swap! state update-in
-                           [:tile/by-index % :value]
-                           (fn [] nil))
-                   [[1 0] [0 1] [1 2] [2 1] [0 0] [2 0] [0 2] [2 2] [1 1]]))})
+  {:action (restart state)})
+
+(defn change-type [state idx]
+  (fn []
+    (swap! state update-in
+           [:player/by-index idx :type]
+           (fn [x]
+             (if (= x :computer)
+               :human
+               :computer)))))
 
 (defmethod mutate 'tic-tac-toe/change-type
   [{:keys [state]} _ {:keys [idx]}]
-  {:action (fn []
-             (swap! state update-in
-                    [:player/by-index idx :type]
-                    (fn [x]
-                      (if (= x :computer)
-                        :human
-                        :computer))))})
+  {:action (change-type state idx)})
